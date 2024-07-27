c46c036305d747037cef9841101259980afbd208
girivivek45 <vivek.21ug2020@iiitranchi.ac.in>
2024-07-27 09:05:08 +0530

    test routes added

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
+import testRoute from "./routes/test.route.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(cookieParser())
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/test", testRoute);
 
 
 app.listen(8800, ()=>{
